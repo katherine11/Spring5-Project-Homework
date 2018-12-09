64dd1f514644a4d48bc1f5ed1ec452bff2aca828
Katerina Simeonova <katherine.simeonova@gmail.com>
2018-12-09 22:50:40 +0200

    Add description

--- src/main/java/com/ksimeonova/simple/blogging/SimpleBloggingApplication/SimpleBloggingApplication.java
+++ src/main/java/com/ksimeonova/simple/blogging/SimpleBloggingApplication/SimpleBloggingApplication.java
@@ -3,6 +3,9 @@ package com.ksimeonova.simple.blogging.SimpleBloggingApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+	This is my project for the Spring5 Course Homeworks
+ */
 @SpringBootApplication
 public class SimpleBloggingApplication {
 
