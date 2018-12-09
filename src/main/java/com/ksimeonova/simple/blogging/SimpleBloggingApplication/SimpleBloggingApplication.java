70edf66922a271e253fa2259f5fe7789bca43a9e
Katerina Simeonova <katherine.simeonova@gmail.com>
2018-12-09 22:41:00 +0200

    Init

---
+++ src/main/java/com/ksimeonova/simple/blogging/SimpleBloggingApplication/SimpleBloggingApplication.java
@@ -0,0 +1,12 @@
+package com.ksimeonova.simple.blogging.SimpleBloggingApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBloggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBloggingApplication.class, args);
+	}
+}
