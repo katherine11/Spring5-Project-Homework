db87cf32db1c2f85f545ccb3eab82d457672d264
Katerina Simeonova <katherine.simeonova@gmail.com>
2019-01-15 02:35:20 +0200

    Create simple UI

--- src/main/java/com/ksimeonova/simple/blogging/SimpleBloggingApplication/controller/BlogController.java
+++ src/main/java/com/ksimeonova/simple/blogging/SimpleBloggingApplication/controller/BlogController.java
@@ -1,13 +1,33 @@
 package com.ksimeonova.simple.blogging.SimpleBloggingApplication.controller;
 
+import com.ksimeonova.simple.blogging.SimpleBloggingApplication.service.BlogServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class BlogController {
 
 
+    @Autowired
+    BlogServiceImpl blogService;
 
+    String index(){
+        return "index";
+    }
 
+    @GetMapping("/blog/create")
+    String create(){
+        return "create";
+    }
+
+    @GetMapping("/blogs")
+    public ModelAndView getBlogs(){
+        ModelAndView blogs = new ModelAndView("blogs");
+        blogs.addObject("blogs", blogService.getFirstFifteen());
+        return blogs;
+    }
 
 
 }
